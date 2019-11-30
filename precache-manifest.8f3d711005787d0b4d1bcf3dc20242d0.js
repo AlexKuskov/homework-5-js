69fa507b71fd48f295fbf9f596eb1f1f0bc1bae1
AlexKuskov <a.kuskov98@gmail.com>
2019-11-30 22:42:13 +0200

    Add build

---
+++ precache-manifest.8f3d711005787d0b4d1bcf3dc20242d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd7dbaaf36b8ede4b8583ae90cd0227a",
+    "url": "/homework-5-js/index.html"
+  },
+  {
+    "revision": "17a76a96b121b97ab78a",
+    "url": "/homework-5-js/static/css/main.05ff66d4.chunk.css"
+  },
+  {
+    "revision": "bc50625d196d5c764edf",
+    "url": "/homework-5-js/static/js/2.437a87d6.chunk.js"
+  },
+  {
+    "revision": "17a76a96b121b97ab78a",
+    "url": "/homework-5-js/static/js/main.b815eed6.chunk.js"
+  },
+  {
+    "revision": "3cc1a72c0e92ca7860a7",
+    "url": "/homework-5-js/static/js/runtime-main.f3807c17.js"
+  }
+]);
