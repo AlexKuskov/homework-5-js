7a3160fe0591c108edd3e4728f711dffc0e7e246
AlexKuskov <a.kuskov98@gmail.com>
2019-11-30 22:11:50 +0200

    Change isStarted state to false by default

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,7 +7,7 @@ class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            isStarted: true,
+            isStarted: false,
             isFinished: false,
             isVictory: false,
         };
